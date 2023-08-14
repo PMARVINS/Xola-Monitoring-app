a261dc3b843fa2108a53795afd6d469869fd32b5
PaulMarvin-Cloud <111180514+PMARVINS@users.noreply.github.com>
2023-08-14 22:21:38 +0000

    added html file, Dockerfile and test results
    having challenges with tests results after running the container, troubleshooting to debug errors. i suspect flask version outdated, i will check that too

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+
+FROM python:3.9-slim-buster
+
+WORKDIR /app
+
+COPY requirements.txt /app/  
+
+RUN pip3 install --no-cache-dir -r requirements.txt
+
+COPY app.py /app/app.py  
+
+ENV FLASK_RUN_HOST=0.0.0.0
+
+EXPOSE 5000
+
+CMD ["flask", "run"]
+
--- app.py
+++ app.py
@@ -1,17 +1,17 @@
-import psutil
-from flask import Flask
 
-app = Flask()
+from flask import Flask, render_template
+import psutil
+
+app = Flask(__name__)
 
 @app.route("/")
 def index():
     cpu_percent = psutil.cpu_percent()
-    mem_percent =psutil.virtual_memory().percent
-    Message = None
+    mem_percent = psutil.virtual_memory().percent
+    message = None
     if cpu_percent > 80 or mem_percent > 80:
-        Message = "High CPU or Memory Utilization detected. Please scale up"
-    return f"CPU Utilization: {cpu_percent} and memonry utilization: {mem_percent}" 
+        message = "High CPU or Memory Utilization detected. Please scale up"
+    return render_template("index.html", cpu_metric=cpu_percent, mem_metric=mem_percent, message=message)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')    
-
+    app.run(debug=True, host='0.0.0.0')
